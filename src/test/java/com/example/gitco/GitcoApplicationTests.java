a9f5385edbda74bb2731d66e30f23a87ea815d44
GitCo <gitcopoc@gmailcom>
2024-05-12 18:23:03 +0530

    Git Co Rest

---
+++ src/test/java/com/example/gitco/GitcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
