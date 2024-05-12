7ee072e05479aa6e221aa0ae4ddbfeb728c9ba30
GitCo <gitcopoc@gmailcom>
2024-05-12 18:48:58 +0530

    Git Co Rest

--- src/main/java/com/example/gitco/GitcoApplication.java
+++ src/main/java/com/example/gitco/GitcoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitcoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GitcoApplication.class, args);
 	}
-
 }
